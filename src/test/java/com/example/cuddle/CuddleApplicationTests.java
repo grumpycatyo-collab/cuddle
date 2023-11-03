5873408f906fb3549a87c82aee8b6eed6cb3f0d7
GrumpyCatYo <linkolnmax24@gmail.com>
2023-11-03 14:28:01 +0200

    Initial commit

---
+++ src/test/java/com/example/cuddle/CuddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
