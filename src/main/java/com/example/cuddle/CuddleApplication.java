ac41ba1de7cb1c666bb6de74986618c010ede2d2
GrumpyCatYo <linkolnmax24@gmail.com>
2023-11-06 00:12:53 +0200

    folder structure

--- src/main/java/com/example/cuddle/CuddleApplication.java
+++ src/main/java/com/example/cuddle/CuddleApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CuddleApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CuddleApplication.class, args);
+
+        System.out.println("mama ta");
     }
 
 }
